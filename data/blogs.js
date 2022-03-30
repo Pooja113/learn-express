2a3ec92ca334237ab9a7386d4a791e9432a1fe50
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2022-03-30 12:51:58 +0530

    Add files via upload

---
+++ data/blogs.js
@@ -0,0 +1,25 @@
+blogs = [
+    {
+    title: "How to learn express",
+    content: "We are going to learn the express js from basic",
+    slug: "express-learn"
+    },
+    {
+      title: "How to learn python",
+      content: "We are going to learn the python from basic",
+      slug: "python-learn"
+    },
+    {
+        title: "How to learn node",
+        content: "We are going to learn the node js from basic",
+        slug: "node-learn"
+    },
+    {
+          title: "How to learn js",
+          content: "We are going to learn the js from basic",
+          slug: "js-learn"
+    }, 
+
+  ]
+
+module.exports = blogs
